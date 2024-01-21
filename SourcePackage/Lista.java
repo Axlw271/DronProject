c6e97a8d271f6f4f3fe18f3b3b975860cfe4e1e0
Axel <axlwallride@gmail.com>
2024-01-21 03:13:33 -0600

    Optimized Lista

--- SourcePackage/Lista.java
+++ SourcePackage/Lista.java
@@ -1,52 +1,47 @@
 package SourcePackage;
 
-class Lista {
-	//variables
-	Nodo inicio;
-	//constructores
-	Lista (int n) {
-		this.inicio = new Nodo(n);
-	}   
-	Lista () {
-		this.inicio = null;
-	}
 
-	//métodos
+class Lista {
+    //variables
+    Nodo inicio;
+    int[] valores; // almacenamos los valores en un array
+    //constructores
+    Lista (int n) {
+        this.inicio = new Nodo(n);
+        this.valores = new int[]{n};
+    }   
+    Lista () {
+        this.inicio = null;
+        this.valores = new int[]{};
+    }
+
+    //métodos
 	void addNodo (int n) {
 		if (inicio != null) {
-			inicio.addFinal(new Nodo(n));			
+			inicio.addFinal(new Nodo(n));   
 		} else {
 			inicio = new Nodo(n);
 		}
+		int[] nuevoArray = new int[valores.length + 1]; // creamos un nuevo array
+		for (int i = 0; i < valores.length; i++) {
+			nuevoArray[i] = valores[i]; // copiamos los elementos del array existente al nuevo array
+		}
+		nuevoArray[nuevoArray.length - 1] = n; // agregamos el nuevo valor al final del nuevo array
+		valores = nuevoArray; // actualizamos la referencia al array
 	}
-	public int getValues(int posicion) {
-		Nodo temp = inicio;
-        int contador = 0;
 
-        while (temp != null) {
-            if (contador == posicion) {
-                return temp.x;
-            }
-            temp = temp.sig;
-            contador++;
+    public int getValues(int posicion) {
+        if (posicion < 0 || posicion >= valores.length) {
+            throw new IndexOutOfBoundsException("La posicón no existe"); //excepción
         }
-		throw new IndexOutOfBoundsException("La posicón no existe"); //excepción
-	} 
+        return valores[posicion];
+    } 
 
-	public int getSize() {
-        int cont = 0;
-        Nodo temp = inicio;
-        while (temp != null) {
-            cont++;
-            temp = temp.sig;
-        }
-        return cont;
+    public int getSize() {
+        return valores.length;
     }
 
-	void print () {
-		inicio.print();
-	}
-
-	
-
+    void print () {
+        inicio.print();
+    }
 }
--- SourcePackage/Nodo.java
+++ SourcePackage/Nodo.java
@@ -1,37 +1,36 @@
 package SourcePackage;
-
 class Nodo {
-	//variables
-	Nodo sig;
-	int x;
-	//constructores
-	Nodo (int x) {
-		this.x = x;
-		this.sig = null;
-	}
-	Nodo (int x, Nodo sig) {
-		this.x = x;
-		this.sig = sig;
-	}
-	//métodos
-	void addFinal(Nodo s) {
-		if(this.sig == null) {
-			this.sig = s;
-		} else {
-			this.sig.addFinal(s);
-		}
-	}
-	void print () {
-		System.out.print(this);
-		if (this.sig != null)
-			sig.print();
-	}
+    //variables
+    Nodo sig;
+    int x;
+    //constructores
+    Nodo (int x) {
+        this.x = x;
+        this.sig = null;
+    }
+    Nodo (int x, Nodo sig) {
+        this.x = x;
+        this.sig = sig;
+    }
+    //métodos
+    void addFinal(Nodo s) {
+        if(this.sig == null) {
+            this.sig = s;
+        } else {
+            this.sig.addFinal(s);
+        }
+    }
+    void print () {
+        System.out.print(this);
+        if (this.sig != null)
+            sig.print();
+    }
 
-	int value () {
-		return x;
-	}
-	@Override
-	public String toString() {
-		return "nodo: " + x;
-	}
+    int value () {
+        return x;
+    }
+    @Override
+    public String toString() {
+        return "nodo: " + x;
+    }
 }
